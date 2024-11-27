6fcdcdd123c94d5a03c17f9ff5357f4df84fa40a
Tanya verma <76943039+TanyaVerma24@users.noreply.github.com>
2024-11-27 21:05:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let boxes = document.querySelectorAll(".box");
+let playBtn = document.getElementById("btn");
+let msg = document.querySelector("#winner");
+let msgContainer = document.querySelector(".msgContainer");
+let currentPlayer = "x";
+let winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerText === "") {
+      console.log(currentPlayer)
+      if (currentPlayer === "x") {
+        box.innerText = "x";
+        box.classList.add("x");
+        box.classList.remove("o")
+        currentPlayer = "o";
+        box.style.backgroundColor = "#ADA0A6";
+      } else {
+        box.innerText = "o";
+        box.classList.add("o");
+        currentPlayer = "x";
+        box.classList.remove("x")
+
+        box.style.backgroundColor = "#ADA0A6";
+      }
+    }
+    checkWinner();
+    // draw();
+  });
+});
+let disabledBox = () => {
+  boxes.forEach((box) => {
+    box.classList.add("disabled");
+  });
+};
+
+checkWinner = () => {
+  for (let pattern of winPatterns) {
+    // console.log(pattern[0] ,pattern[1])
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val === pos2val && pos2val === pos3val) {
+        console.log("winner", pos1val);
+        showWinner(pos1val);
+
+      }
+
+    }
+  }
+};
+playBtn.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.innerText = " ";
+    box.style.backgroundColor = "";
+    box.classList.remove("disabled");
+    msgContainer.classList.add("hide");
+    currentPlayer="x"
+
+  });
+});
+const showWinner = (winner) => {
+  msg.innerText = `Congarts 🎉!! Winner is ${winner} `;
+  msgContainer.classList.remove("hide");
+  disabledBox();
+};
